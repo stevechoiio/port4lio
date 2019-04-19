a8ea4fd9d55488fbf3bab4fb94a800383fcf18bb
Steve Choi <stevechoi93@gmail.com>
2019-04-19 15:44:32 -0700

    git stash apply

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,99 +1,105 @@
-import React, { Component } from 'react';
-export default  class Resume extends Component {
+import React, { Component } from "react";
+export default class Resume extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <section id="resume">
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>Education</span>
+            </h1>
+          </div>
 
-         <div className="row education">
+          <div className="nine columns main-col">
+            {resumeData.education &&
+              resumeData.education.map(item => {
+                return (
+                  <div className="row item">
+                    <div className="twelve columns">
+                      <a href={item.UniversityURL}>
+                        <h3>{item.UniversityName}</h3>
+                      </a>
 
-            <div className="three columns header-col">
-               <h1><span>Education</span></h1>
-            </div>
-
-            <div className="nine columns main-col">
-              {
-                resumeData.education && resumeData.education.map((item)=>{
-                  return(
-                    <div className="row item">
-                       <div className="twelve columns">
-                          <h3>{item.UniversityName}</h3>
-                          <p className="info">
-                          {item.specialization}
-                          <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
-                          <p>
-                          {item.Achievements}
-                          </p>
-                       </div>
+                      <p className="info">
+                        {item.specialization}
+                        <span>&bull;</span>{" "}
+                        <em className="date">
+                          {item.MonthOfPassing} {item.YearOfPassing}
+                        </em>
+                      </p>
+                      <p>{item.Achievements}</p>
                     </div>
-                  )
-                })
-              }
-            </div>
-         </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
         <div className="row work">
-            <div className="three columns header-col">
-               <h1><span>Work</span></h1>
-            </div>
+          <div className="three columns header-col">
+            <h1>
+              <span>Work</span>
+            </h1>
+          </div>
 
-            <div className="nine columns main-col">
-              {
-                resumeData.work && resumeData.work.map((item) => {
-                  return(
-                    <div className="row item">
-                       <div className="twelve columns">
+          <div className="nine columns main-col">
+            {resumeData.work &&
+              resumeData.work.map(item => {
+                return (
+                  <div className="row item">
+                    <div className="twelve columns">
+                      {item.CompanyName == "TalentMarketPlace" ? (
+                        <a href="https://www.talentmarketplace.ca/">
                           <h3>{item.CompanyName}</h3>
-                          <p className="info">
-                          {item.specialization}
-                          <span>&bull;</span> <em className="date">{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
-                          <p>
-                          {item.Achievements}
-                          </p>
-                       </div>
+                        </a>
+                      ) : (
+                        <a href="http://www.standrewkim.com/main/frame.php">
+                          <h3>{item.CompanyName}</h3>
+                        </a>
+                      )}
 
+                      <p className="info">
+                        {item.specialization}
+                        <span>&bull;</span>{" "}
+                        <em className="date">
+                          {item.MonthOfLeaving} {item.YearOfLeaving}
+                        </em>
+                      </p>
+                      <p>{item.Achievements}</p>
                     </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
 
-                  )
-                })
-              }
-            </div> 
-         </div>
+        <div className="row skill">
+          <div className="three columns header-col">
+            <h1>
+              <span>Skills</span>
+            </h1>
+          </div>
 
+          <div className="nine columns main-col">
+            <p>{resumeData.skillsDescription}</p>
 
-         <div className="row skill">
-
-            <div className="three columns header-col">
-               <h1><span>Skills</span></h1>
-            </div>
-
-            <div className="nine columns main-col">
-
-               <p>
-               {resumeData.skillsDescription}
-               </p>
-
-   				<div className="bars">
-
-   				   <ul className="skills">
-                {
-                  resumeData.skills && resumeData.skills.map((item) => {
-                    return(
+            <div className="bars">
+              <ul className="skills">
+                {resumeData.skills &&
+                  resumeData.skills.map(item => {
+                    return (
                       <li>
-                      <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
-                      </span><em>{item.skillname}</em>
+                        <span
+                          className={`bar-expand ${item.skillname.toLowerCase()}`}
+                        />
+                        <em>{item.skillname}</em>
                       </li>
-                    )
-                  })
-                }
-
-   					</ul>
-
-   				</div>
-
-   			</div>
-
-         </div>
-
+                    );
+                  })}
+              </ul>
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,118 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  imagebaseurl: "../bg.jpg",
+  name: "Steve Choi",
+  role: "Full-stack Developer",
+  linkedinId: "stevechoi93",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/stevechoi93/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "http://github.com/stevechoiio",
+      className: "fa fa-github"
+    },
+    {
+      name: "email",
+      url: "mailto:stevechoi93",
+      className: "fa fa-envelope"
+    }
+  ],
+  aboutme:
+    "I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "India",
+  website: "https://rbhatia46.github.io",
+  education: [
+    {
+      UniversityName: "Red Academy",
+      UniversityURL: "https://redacademy.com/vancouver/",
+      specialization: "Full-stack Application Development",
+      MonthOfPassing: "April",
+      YearOfPassing: "2019",
+      Achievements: "Professional Diploma"
+    },
+    {
+      UniversityName: "University of British Columbia",
+      UniversityURL: "https://www.ubc.ca/",
+      specialization: "Major in Philosophy",
+      MonthOfPassing: "May",
+      YearOfPassing: "2017",
+      Achievements: "Bachelor of Arts"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "TalentMarketPlace",
+      specialization: "Software Engineer Intern",
+      MonthOfLeaving: "",
+      YearOfLeaving: "2019",
+      Achievements: ""
+    },
+    {
+      CompanyName: "St.Andrew's Community Centre",
+      specialization: "Assistant Python Instructor",
+      MonthOfLeaving: "",
+      YearOfLeaving: "2018",
+      Achievements: ""
+    }
+  ],
+  skillsDescription: "",
+  skills: [
+    {
+      skillname: "Node"
+    },
+    {
+      skillname: "React"
+    },
+    {
+      skillname: "Python"
+    },
+    {
+      skillname: "AWS"
+    },
+    {
+      skillname: "GraphQL"
+    },
+    {
+      skillname: "mongoDB"
+    },
+    {
+      skillname: "PostgreSQL"
+    }
+  ],
+  portfolio: [
+    {
+      name: "Cool IT!",
+      description: "mobileapp",
+      imgurl: "images/portfolio/coolitlogo.jpg"
+    },
+    {
+      name: "Loyal Flush",
+      description: "mobileapp",
+      imgurl: "images/portfolio/loyalflush.jpg"
+    },
+
+    {
+      name: "FitGO",
+      description: "mobileapp",
+      imgurl: "images/portfolio/dark-logo.svg"
+    }
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    }
+  ]
+};
+
+export default resumeData;
